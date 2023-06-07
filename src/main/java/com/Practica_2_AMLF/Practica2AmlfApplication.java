b23ce5542f6605d36bc818fd9cc0129fc68346a6
2LealFlores9 <114177211+2LealFlores9@users.noreply.github.com>
2023-06-06 22:49:38 -0600

    Tarea Avnace #1

---
+++ src/main/java/com/Practica_2_AMLF/Practica2AmlfApplication.java
@@ -0,0 +1,13 @@
+package com.Practica_2_AMLF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2AmlfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2AmlfApplication.class, args);
+	}
+
+}
